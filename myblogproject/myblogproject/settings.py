312f48e185e16ef7833fcd40b54d6b9a2992f70c
TewodrosAdimas <yosefenadagim@gmail.com>
2025-01-28 11:24:23 +0300

    settings setupped

--- myblogproject/myblogproject/settings.py
+++ myblogproject/myblogproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "blogs",
     "users",
+    "blog_interactions",
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
--- myblogproject/myblogproject/urls.py
+++ myblogproject/myblogproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myblogproject project.
+URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users", include("users.urls")),
+    path("blog", include('blogs.urls')),
+    path('interactions/', include('blog_interactions.urls')), 
+
 ]
