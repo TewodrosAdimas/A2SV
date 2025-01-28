5adfcca11df29e0a6174fe48ce77359695476964
TewodrosAdimas <yosefenadagim@gmail.com>
2025-01-28 11:09:48 +0300

    user serializer  created

---
+++ myblogproject/users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Follow
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'name', 'bio', 'profile_picture', 'role']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError("Passwords must match")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = ['user', 'followed_user']
