445fb4f36bcfc1d36f51f824c7d63c405e095394
TewodrosAdimas <yosefenadagim@gmail.com>
2025-01-28 11:18:02 +0300

    view and url for posts set upped

--- myblogproject/blogs/views.py
+++ myblogproject/blogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Blog
+from .serializers import BlogSerializer
 
-# Create your views here.
+# View to list and create blog posts
+class BlogListCreateView(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Require authentication for creating posts
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+# View to retrieve a specific blog post
+class BlogDetailView(generics.RetrieveAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
