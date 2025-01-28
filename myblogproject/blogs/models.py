7d64fd9b4444063e82326ca952d7fdca81dd501d
TewodrosAdimas <yosefenadagim@gmail.com>
2025-01-28 11:17:08 +0300

    model and serializer for posts set upped

---
+++ myblogproject/blogs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Get the user model to associate blog posts with users
+User = get_user_model()
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, related_name='blogs', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- myblogproject/myblogproject/settings.py
+++ myblogproject/myblogproject/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "blogs",
     "users",
-
 ]
 
 MIDDLEWARE = [
@@ -125,7 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
